555b5db14f931e3e55cfb1981c583f325ee3cc56
Nahid Aghababaeyan <nahid.aut@gmail.com>
2023-04-18 10:25:53 -0400

    model

--- network.py
+++ network.py
@@ -10,3 +10,36 @@ y = x + 0.3 * np.sin(2 * np.pi * (x + ε)) + 0.3 * np.sin(4 * np.pi * (x + ε)) 
 fig, ax = plt.subplots(figsize=(10, 5))
 ax.plot(x, y, 'o', markersize=1);
 
+## Model
+
+import pyro
+import pyro.distributions as dist
+from pyro.nn import PyroModule, PyroSample
+import torch.nn as nn
+from pyro.infer.autoguide import AutoDiagonalNormal
+from pyro.infer import SVI, Trace_ELBO, Predictive
+from tqdm.auto import trange, tqdm
+
+class Model(PyroModule):
+    def __init__(self, h1=20, h2=20):
+        super().__init__()
+        self.fc1 = PyroModule[nn.Linear](1, h1)
+        self.fc1.weight = PyroSample(dist.Normal(0., 1.).expand([h1, 1]).to_event(2))
+        self.fc1.bias = PyroSample(dist.Normal(0., 1.).expand([h1]).to_event(1))
+        self.fc2 = PyroModule[nn.Linear](h1, h2)
+        self.fc2.weight = PyroSample(dist.Normal(0., 1.).expand([h2, h1]).to_event(2))
+        self.fc2.bias = PyroSample(dist.Normal(0., 1.).expand([h2]).to_event(1))
+        self.fc3 = PyroModule[nn.Linear](h2, 1)
+        self.fc3.weight = PyroSample(dist.Normal(0., 1.).expand([1, h2]).to_event(2))
+        self.fc3.bias = PyroSample(dist.Normal(0., 1.).expand([1]).to_event(1))
+        self.relu = nn.ReLU()
+
+    def forward(self, x, y=None):
+        x = x.reshape(-1, 1)
+        x = self.relu(self.fc1(x))
+        x = self.relu(self.fc2(x))
+        mu = self.fc3(x).squeeze()
+        sigma = pyro.sample("sigma", dist.Uniform(0., 1.))
+        with pyro.plate("data", x.shape[0]):
+            obs = pyro.sample("obs", dist.Normal(mu, sigma), obs=y)
+        return mu
