e6eccd142b134cce37ee768063a8b96e5b71d350
Nahid Aghababaeyan <nahid.aut@gmail.com>
2023-04-18 10:22:47 -0400

    data

--- network.py
+++ network.py
@@ -2,3 +2,11 @@ import torch
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+
+x = np.linspace(0, 0.5, 1000)
+ε = 0.02 * np.random.randn(x.shape[0])
+y = x + 0.3 * np.sin(2 * np.pi * (x + ε)) + 0.3 * np.sin(4 * np.pi * (x + ε)) + ε
+
+fig, ax = plt.subplots(figsize=(10, 5))
+ax.plot(x, y, 'o', markersize=1);
+
